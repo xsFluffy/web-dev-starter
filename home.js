def4f4d4401cc88f1752e6a4142e8025421b2e68
xsfluffy <pink-black@hotmail.nl>
2023-09-04 15:00:49 +0200

    4/9

--- home.js
+++ home.js
@@ -1,1 +1,14 @@
-showMessage("GET A GRIP");
+// let price = 99.99, 
+// name='Hiking Boots', 
+// discounted = false;
+
+const price = 40;
+price = 49.99;
+
+showMessage(price); 
+
+
+// single line comment
+/*
+Multi-line comment 
+*/
